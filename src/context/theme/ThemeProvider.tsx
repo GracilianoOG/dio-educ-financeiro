5b607135623a9041a47b720e99acdb1c780fcab2
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-11 16:54:18 -0300

    Get stored theme from storage

--- src/context/theme/ThemeProvider.tsx
+++ src/context/theme/ThemeProvider.tsx
@@ -3,7 +3,15 @@ import { type PropsWithChildren, useEffect, useState } from 'react'
 import { type Theme, ThemeContext } from './ThemeContext'
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
-  const [theme, setTheme] = useState<Theme>('light')
+  const [theme, setTheme] = useState<Theme>(() => {
+    const storedTheme = localStorage.getItem('theme') as Theme | null
+
+    if (storedTheme) {
+      return storedTheme
+    }
+
+    return 'light'
+  })
 
   useEffect(() => {
     document.documentElement.setAttribute('data-theme', theme)
