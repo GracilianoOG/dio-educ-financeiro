5e5c6efb9ff6c413d5ac7ae30939ddd9c4a032b2
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-11 16:16:14 -0300

    Add logo to header

--- src/components/shared/Header.tsx
+++ src/components/shared/Header.tsx
@@ -1,3 +1,19 @@
+import { Wallet } from 'lucide-react'
+
 export const Header = () => {
-  return <div>Header</div>
+  return (
+    <header className="border-b border-(--border) px-6 py-3">
+      <nav className="flex items-center justify-between">
+        <div className="flex items-center gap-2">
+          <div className="bg-primary flex h-9 w-9 items-center justify-center rounded-full">
+            <Wallet size={20} className="text-primary-foreground" />
+          </div>
+          <span className="text-lg">
+            <span className="text-muted-foreground font-medium">Planej</span>
+            <span className="font-extrabold">.ai</span>
+          </span>
+        </div>
+      </nav>
+    </header>
+  )
 }
