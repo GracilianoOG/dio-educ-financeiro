88ecf0e9b92acdbcc35309cd5ec5f7a9d20cf317
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-11 16:51:58 -0300

    Set theme attribute on document element

--- src/context/theme/ThemeProvider.tsx
+++ src/context/theme/ThemeProvider.tsx
@@ -1,9 +1,14 @@
-import { type PropsWithChildren, useState } from 'react'
+import { type PropsWithChildren, useEffect, useState } from 'react'
 
 import { type Theme, ThemeContext } from './ThemeContext'
 
 export const ThemeProvider = ({ children }: PropsWithChildren) => {
   const [theme, setTheme] = useState<Theme>('light')
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme)
+    localStorage.setItem('theme', theme)
+  }, [theme])
 
   const toggleTheme = () => {
     setTheme((scheme) => (scheme === 'light' ? 'dark' : 'light'))
