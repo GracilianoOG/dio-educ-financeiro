9c8cf41fbdb06820bc2ca7170fa78ed13855609b
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-13 11:41:44 -0300

    Use state to set and get current simulation step

--- src/components/features/Simulation/Form.tsx
+++ src/components/features/Simulation/Form.tsx
@@ -1,13 +1,17 @@
+import { useState } from 'react'
+
 import { FormStep } from '@/components/features/Simulation/FormStep'
 import { StepProgress } from '@/components/features/Simulation/Progress'
 import { simulationFormSteps } from '@/data/simulation'
 
 export const SimulationForm = () => {
-  const currentStep = simulationFormSteps[0]
+  const [currentStepIndex, setCurrentStepIndex] = useState(0)
+  const totalSteps = simulationFormSteps.length
+  const currentStep = simulationFormSteps[currentStepIndex]
 
   return (
     <>
-      <StepProgress currentStep={1} totalSteps={6} />
+      <StepProgress currentStep={currentStepIndex + 1} totalSteps={totalSteps} />
       <FormStep key={currentStep.id} {...currentStep} />
     </>
   )
