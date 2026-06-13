8b57e3903d8c6df6b51d8567810488f9c50a937e
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-13 11:49:16 -0300

    Add back and next buttons to form step

--- src/components/features/Simulation/FormStep.tsx
+++ src/components/features/Simulation/FormStep.tsx
@@ -1,4 +1,5 @@
 import { ArrowLeft, ArrowRight, type LucideIcon } from 'lucide-react'
+import type { SyntheticEvent } from 'react'
 
 import { Button } from '@/components/shared/Button'
 import { Input, type InputProps } from '@/components/shared/Input'
@@ -15,13 +16,26 @@ export interface FormStepProps {
   }
 }
 
+interface ActionsButtonsProps {
+  onBack: () => void
+  onNext: () => void
+  hideBackButton?: boolean
+}
+
 export const FormStep = ({
   icon: Icon,
   title,
   question,
   inputProps,
   submitButtonProps,
-}: FormStepProps) => {
+  onBack,
+  onNext,
+}: FormStepProps & ActionsButtonsProps) => {
+  const handleSubmit = (e: SyntheticEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    onNext()
+  }
+
   return (
     <div className="bg-card rounded-2xl p-6 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)] sm:p-8">
       <div className="bg-primary mb-4 flex h-15 w-15 items-center justify-center rounded-xl">
@@ -31,11 +45,12 @@ export const FormStep = ({
       <h3 className="text-foreground mb-6 text-xl leading-snug font-semibold sm:text-2xl">
         {question}
       </h3>
-      <form className="flex flex-col gap-4">
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
         <Input {...inputProps} />
         <div className="flex flex-col gap-3 sm:flex-row sm:gap-6">
           <Button
             type="button"
+            onClick={onBack}
             variant="ghost"
             icon={ArrowLeft}
             className="order-2 flex-1 justify-center rounded-xl py-3 sm:order-1"
