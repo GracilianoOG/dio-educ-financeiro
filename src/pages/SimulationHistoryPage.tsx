05d88a8f839ae80f0632c869027a92b6e61df584
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-29 17:10:52 -0300

    Add container to history cards

--- src/pages/SimulationHistoryPage.tsx
+++ src/pages/SimulationHistoryPage.tsx
@@ -2,11 +2,12 @@ import { PageHero } from '@/components/shared/PageHero'
 
 export const SimulationHistoryPage = () => {
   return (
-    <div className="px-4 py-8">
+    <div className="mb-24px px-4 py-8">
       <PageHero
         title="Histórico de simulações"
         subtitle="Com base no seu perfil financeiro e objetivos."
       />
+      <div></div>
     </div>
   )
 }
