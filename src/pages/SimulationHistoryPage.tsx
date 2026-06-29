735584cc0aabec4c7af960b3797053015e2145c5
Gabriel <gabriel.gracilianomb@gmail.com>
2026-06-29 17:09:00 -0300

    Set padding of history page

--- src/pages/SimulationHistoryPage.tsx
+++ src/pages/SimulationHistoryPage.tsx
@@ -2,7 +2,7 @@ import { PageHero } from '@/components/shared/PageHero'
 
 export const SimulationHistoryPage = () => {
   return (
-    <div>
+    <div className="px-4 py-8">
       <PageHero
         title="Histórico de simulações"
         subtitle="Com base no seu perfil financeiro e objetivos."
