7e344784727f61f00f69908a4e6a437abc2d6a39
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-01 18:43:01 -0300

    Change div to main

--- src/pages/SimulationHistoryPage.tsx
+++ src/pages/SimulationHistoryPage.tsx
@@ -4,7 +4,7 @@ import { PageHero } from '@/components/shared/PageHero'
 
 export const SimulationHistoryPage = () => {
   return (
-    <div className="mb-24px px-4 py-8">
+    <main className="mb-24px px-4 py-8">
       <PageHero
         title="Histórico de simulações"
         subtitle="Com base no seu perfil financeiro e objetivos."
@@ -20,6 +20,6 @@ export const SimulationHistoryPage = () => {
           </div>
         </div>
       </div>
-    </div>
+    </main>
   )
 }
