c60964750f8c5b40ed0330c941b2e5f87de15948
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-02 13:22:59 -0300

    Create function to switch plural of word month

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,3 +1,7 @@
 export const formatDate = (date: string) => {
   return new Date(date).toLocaleDateString('pt-BR')
 }
+
+export const switchMonthPlural = (months: string | number) => {
+  return Number(months) > 1 ? 'meses' : 'mês'
+}
