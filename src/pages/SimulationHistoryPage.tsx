1c75e13d7a2246464e0cd96fc2e4b48e7ea56e97
Gabriel <gabriel.gracilianomb@gmail.com>
2026-07-02 00:23:08 -0300

    Generate card list for each insight stored

--- src/pages/SimulationHistoryPage.tsx
+++ src/pages/SimulationHistoryPage.tsx
@@ -1,10 +1,18 @@
-import { ExternalLink, Goal, Trash2 } from 'lucide-react'
+import { useState } from 'react'
 
-import { Button } from '@/components/shared/Button'
-import { Divider } from '@/components/shared/Divider'
+import Card from '@/components/features/History/Card'
 import { PageHero } from '@/components/shared/PageHero'
+import { useSimulationStorage } from '@/hooks/useSimulationStorage'
 
 export const SimulationHistoryPage = () => {
+  const { deleteFormData, getAllFormData } = useSimulationStorage()
+  const [insights, setInsights] = useState(() => getAllFormData())
+
+  const handleDelete = (id: string) => {
+    deleteFormData(id)
+    setInsights(getAllFormData())
+  }
+
   return (
     <main className="mb-24px px-4 py-8">
       <PageHero
@@ -12,41 +20,18 @@ export const SimulationHistoryPage = () => {
         subtitle="Com base no seu perfil financeiro e objetivos."
       />
       <div>
-        <div className="bg-card flex flex-col gap-6 rounded-[22px] p-8 shadow-[4px_4px_18px_0px_rgba(0,0,0,0.2)]">
-          <div className="flex size-10 items-center justify-center rounded-[10.67px] bg-[#ECE5F8]">
-            <Goal size={26} className="text-primary" />
-          </div>
-          <div>
-            <p className="text-foreground mb-1 text-base font-semibold">Viagem para o Japão</p>
-            <p className="text-muted-foreground text-sm">24/02/2026</p>
-          </div>
-          <div>
-            <p className="text-muted-foreground mb-1 text-xs font-semibold uppercase">
-              Custo da meta
-            </p>
-            <p className="text-foreground text-base font-semibold">R$ 15.000,00</p>
-          </div>
-          <div>
-            <p className="text-muted-foreground mb-1 text-xs font-semibold uppercase">Prazo</p>
-            <p className="text-foreground text-base font-semibold">12 meses</p>
-          </div>
-          <div>
-            <p className="text-muted-foreground mb-1 text-xs font-semibold uppercase">
-              Economia mensal
-            </p>
-            <p className="text-foreground text-base font-semibold">R$ 1.250,00</p>
-          </div>
-          <Divider spacing={0} />
-          <div className="flex flex-row justify-between">
-            <Button className="flex-1" variant="ghost">
-              <Trash2 className="text-red-500" size={24} />
-            </Button>
-            <Divider orientation="vertical" />
-            <Button className="flex-1 text-nowrap" icon={ExternalLink} variant="ghost">
-              Ver detalhes
-            </Button>
-          </div>
-        </div>
+        {insights?.map((data) => (
+          <Card
+            key={data.id}
+            id={data.id}
+            goalName={data.goalName}
+            date={'01/01/2000'}
+            goalCost={data.goalAmount}
+            months={data.goalDeadline}
+            savings={data.income}
+            onDelete={() => handleDelete(data.id)}
+          />
+        ))}
       </div>
     </main>
   )
